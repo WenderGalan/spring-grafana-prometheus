1664ddbe8544dcafbab182823998ebf90396247e
Wender Galan <wendergalan2014@hotmail.com>
2021-02-15 18:15:04 -0400

    Criando o projeto.

---
+++ src/main/java/io/github/wendergalan/springgrafanaprometheus/SpringGrafanaPrometheusApplication.java
@@ -0,0 +1,13 @@
+package io.github.wendergalan.springgrafanaprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrafanaPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrafanaPrometheusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wendergalan/springgrafanaprometheus/SpringGrafanaPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wendergalan.springgrafanaprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrafanaPrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
