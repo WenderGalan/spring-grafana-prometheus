71dc2d2641ced9d24821d6bcdd01ec89f41ad99f
Wender Galan <wendergalan2014@hotmail.com>
2021-02-15 19:27:13 -0400

    Adicionando o grafana ao projeto.

--- src/main/java/io/github/wendergalan/springgrafanaprometheus/SpringGrafanaPrometheusApplication.java
+++ src/main/java/io/github/wendergalan/springgrafanaprometheus/SpringGrafanaPrometheusApplication.java
@@ -1,7 +1,11 @@
 package io.github.wendergalan.springgrafanaprometheus;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringGrafanaPrometheusApplication {
@@ -10,4 +14,8 @@ public class SpringGrafanaPrometheusApplication {
 		SpringApplication.run(SpringGrafanaPrometheusApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
